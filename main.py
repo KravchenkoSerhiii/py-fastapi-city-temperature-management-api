9f09ded42882b5f39dce577bccc09f61fc1e297e
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-07-17 15:50:46 +0200

    Changes

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ from city_temperature import router as city_temperature_router
 app = FastAPI()
 
 app.include_router(city_temperature_router.router)
-
-
-@app.get("/")
-def root() -> dict:
-    return {"message": "Hello World"}
